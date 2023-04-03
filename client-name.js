b57bfbc33d10aeb155f26663022261049e8664c0
Rich Malloy <ahoymalloy@gmail.com>
2023-04-03 14:46:58 -0500

    Create client-name.js

---
+++ client-name.js
@@ -0,0 +1,11 @@
+<script aria-hidden="true" type= "text/javascript">
+  (function () {
+    var a = document.createElement('script');
+    var b = document.getElementsByTagName('script')[0];
+    a.type = 'text/javascript';
+    a.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'js.talkfurther.com/talkfurther_init.min.js';
+    a.async = true;
+    b.parentNode.insertBefore(a, b);
+  }
+  )();
+</script>
